4d2aa5f4831de638bebb83f15fb47b5d1b68e0d2
Russ Cox <rsc@golang.org>
2023-12-12 17:57:18 -0500

    markdown: move entity table into its own file

--- entity2go.go
+++ entity2go.go
@@ -8,16 +8,24 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"flag"
 	"fmt"
+	"go/format"
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"sort"
 	"strings"
 )
 
+var outfile = flag.String("o", "", "write output to `file`")
+
 func main() {
+	flag.Parse()
+
 	resp, err := http.Get("https://html.spec.whatwg.org/entities.json")
 	if err != nil {
 		log.Fatal(err)
@@ -44,20 +52,46 @@ func main() {
 	}
 	sort.Strings(names)
 
-	fmt.Printf("var htmlEntity = map[string]string{\n")
+	var buf bytes.Buffer
+	buf.WriteString(hdr)
+	fmt.Fprintf(&buf, "var htmlEntity = map[string]string{\n")
 	for _, name := range names {
 		if !strings.HasSuffix(name, ";") {
 			continue
 		}
-		fmt.Printf("\t%q: \"", name)
+		fmt.Fprintf(&buf, "\t%q: \"", name)
 		for _, r := range list[name].Codepoints {
 			if r <= 0xFFFF {
-				fmt.Printf("\\u%04x", r)
+				fmt.Fprintf(&buf, "\\u%04x", r)
 			} else {
-				fmt.Printf("\\U%08x", r)
+				fmt.Fprintf(&buf, "\\U%08x", r)
 			}
 		}
-		fmt.Printf("\",\n")
+		fmt.Fprintf(&buf, "\",\n")
 	}
-	fmt.Printf("}\n")
+	fmt.Fprintf(&buf, "}\n")
+
+	src, err := format.Source(buf.Bytes())
+	if err != nil {
+		log.Fatalf("reformatting output: %v", err)
+	}
+
+	if *outfile != "" {
+		if err := os.WriteFile(*outfile, src, 0666); err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		os.Stdout.Write(buf.Bytes())
+	}
 }
+
+var hdr = `// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:generate go run entity2go.go -o entity.go
+
+package markdown
+
+// htmlEntity maps known HTML entity sequences to their meanings.
+`
